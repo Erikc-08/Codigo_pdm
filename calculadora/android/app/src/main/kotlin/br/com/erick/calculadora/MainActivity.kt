83bc91df97debb6b53ce14d88c6ca17fbdcac5fe
Erick <erickmarcial085@gmail.com>
2026-04-24 12:46:53 -0300

    primeiro

---
+++ calculadora/android/app/src/main/kotlin/br/com/erick/calculadora/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.erick.calculadora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ calculadora/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:calculadora/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
