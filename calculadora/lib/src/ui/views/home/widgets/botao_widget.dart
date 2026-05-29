6c1c54b17083031bff9c046da20a06908baa23b5
Erikc-08 <erickmarcial085@gmail.com>
2026-05-29 11:48:34 -0300

    Update botao_widget.dart

--- calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
+++ calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
@@ -5,6 +5,9 @@ class Botao extends StatelessWidget {
 
 @override
 Widget build(BuildContext context) {
-  return const Placeholder();
+  return FilledButton(
+    onPressed: (){},
+    child: Text("9"),
+    );
   }
 }
