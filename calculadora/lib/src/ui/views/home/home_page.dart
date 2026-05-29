36e742f170ca256325f085bb3bc66da6dc9edf8f
Erikc-08 <erickmarcial085@gmail.com>
2026-05-29 12:26:00 -0300

    Update home_page.dart

--- calculadora/lib/src/ui/views/home/home_page.dart
+++ calculadora/lib/src/ui/views/home/home_page.dart
@@ -30,7 +30,7 @@ class _HomePageState extends State<HomePage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-            Botao(texto: "9",),
+            Botao(texto: "9",onPressed: (){},),
             TextButton(onPressed: (){}, child: Text('9')),
             TextButton(onPressed: (){}, child: Text('9')),
             TextButton(onPressed: (){}, child: Text('9'))
