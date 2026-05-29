cc5e9984817f04d1c6ca8a1bf9245eb356e75f1d
Erikc-08 <erickmarcial085@gmail.com>
2026-05-29 12:03:43 -0300

    Update botao_widget.dart

--- calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
+++ calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
@@ -7,7 +7,19 @@ class Botao extends StatelessWidget {
 Widget build(BuildContext context) {
   return FilledButton(
     onPressed: (){},
-    child: Text("9"),
+    style: FilledButton.styleFrom(
+      backgroundColor: const Color.fromARGB(255, 54, 54, 54),
+      fixedSize: Size(60, 60),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50)
+      )
+    ),
+     child: Text("9",
+     style: TextStyle(
+     fontSize: 20,
+     fontWeight:  FontWeight.bold,
+     color: Colors.white
+     )),
     );
   }
 }
