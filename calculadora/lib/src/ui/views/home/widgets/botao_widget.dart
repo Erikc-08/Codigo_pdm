b2baa69d629e300fd81c837cddfc56e09d2868c7
Erikc-08 <erickmarcial085@gmail.com>
2026-06-12 11:47:03 -0300

    Update botao_widget.dart

--- calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
+++ calculadora/lib/src/ui/views/home/widgets/botao_widget.dart
@@ -4,25 +4,30 @@ class Botao extends StatelessWidget {
   final String texto;
   final void Function()? onPressed;
   final Color? backgroundColor;
-  const Botao({super.key, required this.texto, this.onPressed, this.backgroundColor});
+  final double width;
+  const Botao({super.key, required this.texto, this.onPressed, 
+  this.backgroundColor, this.width = 60});
 
 @override
 Widget build(BuildContext context) {
-  return FilledButton(
-    onPressed: onPressed,
-    style: FilledButton.styleFrom(
-      backgroundColor: backgroundColor ?? const Color.fromARGB(255, 54, 54, 54),
-      fixedSize: Size(60, 60),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(50)
-      )
-    ),
-     child: Text(texto,
-     style: TextStyle(
-     fontSize: 20,
-     fontWeight:  FontWeight.bold,
-     color: Colors.white
-     )),
-    );
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 10.0),
+    child: FilledButton(
+      onPressed: onPressed,
+      style: FilledButton.styleFrom(
+        backgroundColor: backgroundColor ?? const Color.fromARGB(255, 54, 54, 54),
+        fixedSize: Size(width, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60)
+        )
+      ),
+       child: Text(texto,
+       style: TextStyle(
+       fontSize: 20,
+       fontWeight:  FontWeight.bold,
+       color: Colors.white
+       )),
+      ),
+  );
   }
 }
